f31ebfa11b1c9b106531ad561c2a599757e519b6
Ami <ami.nakano@outlook.com>
2019-10-31 22:50:57 +0100

    random page works

--- src/views/Random.vue
+++ src/views/Random.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="random">
-    <h1>This is a random page</h1>
+    <!-- <h1>This is a random page</h1> -->
     <div>
       <h2 class="question">
         {{decodeHTMLEntities(quiz.question)}} 
       </h2>
+      <div class="result">
+        <p v-if="correct" class="result__positive">Correct!</p>
+        <p v-if="incorrect" class="result__negative">Incorrect...</p>
+        <div v-if="isLoaded" class="lds-ripple"><div></div><div></div></div>
+      </div>
+
       <div class="labels">
         <span class="labels__label">{{quiz.category}}</span>
         <span class="labels__label">{{quiz.difficulty}}</span>
@@ -12,7 +18,8 @@
       <div class="multiples">
           <div 
             v-for="choice in multiple"
-            v-bind:key = "choice">
+            v-bind:key= "choice"
+            v-on:click="isCorrect(choice)">
             {{choice}}
           </div>
       </div>  
@@ -26,11 +33,15 @@ import axios from 'axios';
 
 @Component
 export default class Random extends Vue {
-    quiz: object = []
-    multiple:object = [];
+    quiz: object = [];
+    multiple: object = [];
+    correct: boolean = false;
+    incorrect: boolean = false;
+    isLoaded: boolean = true;
 
   mounted(){
     this.getQuizzes()
+    
   }
   async getQuizzes(){
     const instance = axios.create({
@@ -40,9 +51,9 @@ export default class Random extends Vue {
     this.quiz = await response.data.results[0]
     this.multiple = this.quiz.incorrect_answers;
     this.multiple.push(this.quiz.correct_answer);
-    // this.multiple = this.quiz.incorrect_answers;
     this.arrShuffle(this.multiple);
-    console.log(this.quiz.correct_answer);
+    this.isLoaded = false
+
   }
   decodeHTMLEntities(text:string) {
    let textArea = document.createElement('textarea');
@@ -57,6 +68,22 @@ export default class Random extends Vue {
         item[num] = item[i];
         item[i] = str;
       }
+  }
+  isCorrect(selected:string){
+
+      if(selected === this.quiz.correct_answer) {
+        this.correct = true
+        setTimeout(()=>{ 
+          this.getQuizzes()
+          this.correct = false
+          this.isLoaded =true
+          },1000);
+
+      }else {
+        this.incorrect = true
+        setTimeout(()=>{this.incorrect = false},500)
+      }
+
   }
 }
 </script>
