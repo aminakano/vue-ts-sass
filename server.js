a5493fdc3c438ad4f74aa31a5e2931e913efd2e1
Ami <ami.nakano@outlook.com>
2019-11-11 17:16:35 +0100

    minor change

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const serveStatic = require("serve-static");
 const path = require("path");
 app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
 const port = process.env.PORT || 80;
 app.listen(port);
